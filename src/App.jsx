2705d6df312796dceadb34c8709efefd7c8fe8ef
Olivia <97416561+olivia497@users.noreply.github.com>
2025-08-05 21:02:03 -0600

    Update App.jsx
    Updated router

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,30 @@ import BookNow from './pages/BookNow'
 import Contact from './pages/Contact'
 import Rooms from './pages/Rooms'
 import Footer from './components/Footer'
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Restaurant from './pages/Restaurant'
 import Event from './pages/Events'
 import ScrollToTop from './components/ScrollToTop'
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter basename="/lume-hotel-project">
       <ScrollToTop />
-        <div className='min-h-screen flex flex-col'>
-          <Navbar />
-          <div className='container flex- grow'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/events' element={<Event />} />
-              <Route path='/restaurant' element={<Restaurant />} />
-              <Route path='/rooms' element={<Rooms />} />
-              <Route path='/contact' element={<Contact />} />
-              <Route path='/booknow' element={<BookNow />} />
-            </Routes>
-          </div>
-          <Footer />
+      <div className='min-h-screen flex flex-col'>
+        <Navbar />
+        <div className='container flex-grow'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/events' element={<Event />} />
+            <Route path='/restaurant' element={<Restaurant />} />
+            <Route path='/rooms' element={<Rooms />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/booknow' element={<BookNow />} />
+          </Routes>
         </div>
-    </>
+        <Footer />
+      </div>
+    </BrowserRouter>
   )
 }
 
